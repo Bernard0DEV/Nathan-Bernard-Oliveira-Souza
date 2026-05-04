85fbaf126a1e966e4c10cbed1eb7d9ff56743dbb
Your Name <you@example.com>
2026-05-04 20:51:51 -0300

    aula sla

---
+++ exercicios/exercicio6.py
@@ -0,0 +1,23 @@
+
+def soma(*args):
+    valor += 0
+    for n in args:
+        return args
+
+def subtração(*args):
+    valor -= 0
+    for n in args:
+        return args
+
+def multiplicação(*args):
+    valor *= 0
+    for n in args:
+        return args
+ 
+def divisão(*args):
+    valor /= 0
+    for n in args:
+        return args
+
+while True:
+    input("Digite um valor")
